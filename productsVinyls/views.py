a95178a0c35e2b3df1af4a9d101816166dddcbdd
CapitainHolmes <thomasjj.holmes@gmail.com>
2020-02-01 18:07:10 +0000

    Created a view inside 'productsVinyls/views.py' to access all vinyls within the database.

--- productsVinyls/views.py
+++ productsVinyls/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Vinyl
 # Create your views here.
+
+# A view to return all vinyls within the database
+def all_vinyls(request):
+    vinyls = Vinyl.objects.all()
+    return render(request, "vinyls.html")
