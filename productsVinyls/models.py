a0ca8a2aae6064f0edda42ecac51f5c647d4853a
CapitainHolmes <thomasjj.holmes@gmail.com>
2020-02-01 17:11:36 +0000

    Updated the model in 'productsVinyls/models.py' to fit with a vinyl object.

--- productsVinyls/models.py
+++ productsVinyls/models.py
@@ -5,7 +5,9 @@ from django.db import models
 # Models for Vinyl Products
 
 class Vinyl(models.Model):
-    name = models.CharField(max_length=254, default='')
+    artist = models.CharField(max_length=254, default='')
+    album = models.CharField(max_length=254, default='')
+    genre = models.CharField(max_length=254, default='')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
