b9d017cb7fc9f6dd738364fafc312fb9f046c0fc
CapitainHolmes <thomasjj.holmes@gmail.com>
2020-02-04 19:06:54 +0000

    Added a view to 'cart/views.py' that will show the quantity of vinyl records a user has added to their cart.

--- cart/views.py
+++ cart/views.py
@@ -3,3 +3,16 @@ from django.shortcuts import render
 # The view for rendering the "cart.html" page
 def view_cart(request):
     return render(request, "cart.html")
+
+# A view to show the quantity of vinyls that a user has in their cart
+def add_to_cart(request, id):
+    quantity = int(request.POST.get('quantity'))
+
+    cart = request.session.get('cart', {})
+    if id in cart:
+        cart[id] = int(cart[id]) + quantity      
+    else:
+        cart[id] = cart.get(id, quantity) 
+
+    request.session['cart'] = cart
+    return redirect(reverse('index'))
