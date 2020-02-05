271312c8c18ece073997600d942f285c9e740913
CapitainHolmes <thomasjj.holmes@gmail.com>
2020-02-05 19:06:59 +0000

    Added the url for the 'cart' app in vinylEcommerce app.

--- vinylEcommerce/urls.py
+++ vinylEcommerce/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     url(r'^$', all_vinyls, name='index'),
     url(r'^accounts/', include(urls_accounts)),
     url(r'^productsVinyls/', include(urls_productsVinyls)),
+    url(r'^cart/', include(urls_cart)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),
 ]
