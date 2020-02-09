d56d58798d69670f37782dadb6ca5467a5ac8713
CapitainHolmes <thomasjj.holmes@gmail.com>
2020-02-09 14:12:05 +0000

    Added a view for the search for vinyls functionality.

--- searchVinyls/views.py
+++ searchVinyls/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from productsVinyls.models import Vinyl
 
 # Create your views here.
+
+# A view for the searching of vinyls funnctionality
+
+def do_search(request):
+    vinyls = Vinyl.objects.filter(name__icontains=request.GET['q'])
+    return render(request, "vinyls.html", {"vinyls": vinyls})
