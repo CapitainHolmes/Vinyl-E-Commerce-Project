129d0268ecba5c0715967e5291705819179baa1b
CapitainHolmes <thomasjj.holmes@gmail.com>
2020-02-11 19:09:26 +0000

    Importing the necessary models for the views in the checkout app.

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import PaymentForm, OrderForm
+from django.conf import settings
+from django.utils import timezone
+from django.contrib import messages
+from products.models import Vinyl
+import stripe
+from .models import OrderLineVinyl
 # Create your views here.
+
