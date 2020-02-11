d4118ce3a8afd0ac3cbf6a5c127532f3bc1577c2
CapitainHolmes <thomasjj.holmes@gmail.com>
2020-02-11 20:00:31 +0000

    Added the full view for the user the checkout and submit an online payment.

--- checkout/views.py
+++ checkout/views.py
@@ -17,3 +17,45 @@ def checkout(request):
     if request.method == "POST":
         order_form = OrderForm(request.POST)
         payment_form = PaymentForm(request.POST)
+
+        if order_form.is_valid() and payment_form.is_valid():
+            order = order_form.save(commit=False)
+            order.date = timezone.now()
+            order.save()
+
+            cart = request.session.get('cart', {})
+            total = 0
+            for id, quantity in cart.items():
+                vinyl = get_object_or_404(Vinyl, pk=id)
+                total += quantity * vinyl.price
+                order_line_vinyl = OrderLineVinyl(
+                    order=order,
+                    vinyl=vinyl,
+                    quantity=quantity
+                )
+                order_line_item.save()
+            
+            try:
+                customer = stripe.Charge.create(
+                    amount=int(total * 100),
+                    currency="GBP",
+                    description=request.user.email,
+                    card=payment_form.cleaned_data['stripe_id']
+                )
+            except stripe.error.CardError:
+                messages.error(request, "Your card was declined!")
+            
+            if customer.paid:
+                messages.error(request, "You have successfully paid")
+                request.session['cart'] = {}
+                return redirect(reverse('productsViynls'))
+            else:
+                messages.error(request, "Unable to take payment")
+        else:
+            print(payment_form.errors)
+            messages.error(request, "We were unable to take a payment with that card!")
+    else:
+        payment_form = PaymentForm()
+        order_form = OrderForm()
+    
+    return render(request, "checkout.html", {"order_form": order_form, "payment_form": payment_form, "publishable": settings.STRIPE_PUBLISHABLE})
