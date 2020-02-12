79929135e2b3e9b90c22d0b17e650d8ce551d47f
CapitainHolmes <thomasjj.holmes@gmail.com>
2020-02-12 16:45:02 +0000

    Created a function in stripe.js, linking to the payment form to transfer data to stripe.

---
+++ static/js/stripe.js
@@ -0,0 +1,11 @@
+// Linking the html and django forms to Stripe's API
+
+$(function() {
+    $("#payment-form").submit(function() {
+        var form = this;
+        var card = {
+            number: $("#id_credit_card_number").val(),
+            expMonth: $("#id_expiry_month").val(),
+            expYear: $("#id_expiry_year").val(),
+            cvc: $("#id_cvv").val()
+        };
