c7ab8ee0f75c7ae7b1c0e382a8d39cf77159d8d9
CapitainHolmes <thomasjj.holmes@gmail.com>
2020-02-22 12:46:29 +0000

    Addiing the storages in settings.py file.

--- vinylEcommerce/settings.py
+++ vinylEcommerce/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'vinylEcommerce.wsgi.application'
 # }
 
 if "DATABASE_URL" in os.environ:
-    
+
     DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 
 else:
@@ -155,10 +155,8 @@ AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-
-
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
 STATIC_URL = '/static/'
 
@@ -172,3 +170,6 @@ STATICFILES_DIRS = (
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
