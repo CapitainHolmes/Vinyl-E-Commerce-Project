1b68ea25511e2be1002f6cec9e14a82a3ebda037
CapitainHolmes <thomasjj.holmes@gmail.com>
2020-02-22 12:41:44 +0000

    Trying to resolve travis ci issue - keeps failing.

--- vinylEcommerce/settings.py
+++ vinylEcommerce/settings.py
@@ -91,7 +91,9 @@ WSGI_APPLICATION = 'vinylEcommerce.wsgi.application'
 # }
 
 if "DATABASE_URL" in os.environ:
+    
     DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+
 else:
     print("Database URL not found. Using SQLite instead")
     DATABASES = {
